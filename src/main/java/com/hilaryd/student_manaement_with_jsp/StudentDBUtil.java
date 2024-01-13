bac981558e26ad8b50a4ebc9fb47674e13148873
Ndifoinhilar@gmail.com <ndifoinhilary@gmail.com>
2024-01-13 17:37:06 +0100

    work on the studentdbutil class that gets the student from the db

---
+++ src/main/java/com/hilaryd/student_manaement_with_jsp/StudentDBUtil.java
@@ -0,0 +1,62 @@
+package com.hilaryd.student_manaement_with_jsp;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDBUtil {
+    private DataSource dataSource;
+
+    StudentDBUtil(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    public List<Students> getStudent() throws Exception {
+
+        List<Students> students = new ArrayList<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+
+            connection = dataSource.getConnection();
+            String sql = "select * from student order by  first_name";
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lasttName = resultSet.getString("last_name");
+                String email = resultSet.getString("email");
+                Students students1 = new Students(id, firstName, lasttName, email);
+                students.add(students1);
+            }
+            return students;
+        } finally {
+            close(connection, statement, resultSet);
+        }
+    }
+        private void close(Connection connection,Statement statement,ResultSet resultSet){
+          try {
+              if (connection != null){
+                  connection.close();
+              }
+              if (statement != null){
+                  statement.close();
+              }
+              if (resultSet != null){
+                  resultSet.close();
+              }
+          }catch (Exception exception){
+              exception.printStackTrace();
+          }
+        }
+
+}
